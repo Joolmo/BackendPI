f1e65b7b5b809b049cba7f1eb4d952ed8f583163
JhernKR <josephernandez108@gmail.com>
2020-02-10 16:40:59 +0100

    Starting token auth

--- BackendPI/Controllers/LoginController.cs
+++ BackendPI/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Threading;
 
 namespace BackendPI.Controllers
 {
@@ -37,6 +38,53 @@ namespace BackendPI.Controllers
                 Data = data
             };
         }
+
+        //PRINCIPIO
+
+        /// <summary>
+        /// login controller class for authenticate users
+        /// </summary>
+        [AllowAnonymous]
+        [RoutePrefix("api/login")]
+        public class LoginController : ApiController
+        {
+            [HttpGet]
+            [Route("echoping")]
+            public IHttpActionResult EchoPing()
+            {
+                return Ok(true);
+            }
+
+            [HttpGet]
+            [Route("echouser")]
+            public IHttpActionResult EchoUser()
+            {
+                var identity = Thread.CurrentPrincipal.Identity;
+                return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+            }
+
+            [HttpPost]
+            [Route("authenticate")]
+            public IHttpActionResult Authenticate(LoginRequest login)
+            {
+                if (login == null)
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+                //TODO: Validate credentials Correctly, this code is only for demo !!
+                bool isCredentialValid = (login.Password == "123456");
+                if (isCredentialValid)
+                {
+                    var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                    return Ok(token);
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+        }
+
+        // FIN
 
         // PUT: api/Login/5
         public void Put(int id, [FromBody]string value)
--- BackendPI/Global.asax.cs
+++ BackendPI/Global.asax.cs
@@ -18,6 +18,8 @@ namespace BackendPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
